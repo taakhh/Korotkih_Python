cf30bac2ff7e1f8b35c2b07f781578c1b9b45cf6
Professional <tania070607kat@gmail.con>
2024-11-28 20:46:54 +0300

    готово

--- pz_5/pz_5_1.py
+++ pz_5/pz_5_1.py
@@ -1,17 +1,21 @@
 ''' С помощью функций получить вертикальную и горизонтальную линии. Линия
 проводится многократной печатью символа. Заключить слово в рамку из
 полученных линий. '''
+
+
 def h_line(length):
   return '-' * length
 
 
 def ramka(word):
   word_length = len(word)
-  vertical_line = ('|' + word + '|')
   horizontal_line = h_line(word_length + 2)
-  print(horizontal_line)
-  print(vertical_line)
-  print(horizontal_line)
+  vertical_line = '|' + word + '|'
+
+  print(horizontal_line)  # Верхняя линия
+  print(vertical_line)  # Слово с вертикальными линиями
+  print(horizontal_line)  # Нижняя линия
+
 
 word = input('Введите слово: ')
 ramka(word)
