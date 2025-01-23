465ea19f25589e1d7398cf28e7181cc0e648c49c
Professional <tania070607kat@gmail.con>
2025-01-23 23:26:15 +0300

    готово

--- pz_9/pz_9_1.py
+++ pz_9/pz_9_1.py
@@ -1,2 +1,15 @@
-# Выполнить сортировку словаря d = {'a': 1, 'b': 2, 'c': 3}
-d = {'a': 1, 'b': 2, 'c': 3 }
+# Организовать словарь avto, содержащий 3 ключа (марки авто) и списки
+# из трех моделей в качестве значений. Обеспечить отображение вторых моделей по
+# каждому авто, всех моделей словаря.
+
+avto = {'Audi': ['Audi RS6', 'Audi A4', 'Audi A8'] ,
+        'BMW': ['BMW X6', 'BMW 3','BMW 5'],
+        'ferrari' : ['Ferrari F40', 'Ferrari Daytona', 'Ferrari 458']}
+
+print('Вторые модели:')
+for marka, model in avto.items():
+  print(f'{marka}: {model[1]}')
+
+print('Все модели: ')
+for marka, model in avto.items():
+  print(f'{marka}: {model}')
