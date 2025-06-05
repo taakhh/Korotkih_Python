49be7ba43ede3dc8237a1b5a480b6fd115f8c60f
tania <tania070607kat@gmail.com>
2025-06-06 02:32:18 +0300

    готово

--- pz_16/pz_16_1.py
+++ pz_16/pz_16_1.py
@@ -6,7 +6,6 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
-
     def display_info(self):
         print(f"Марка: {self.make}, Модель: {self.model}, Год выпуска: {self.year}")
 
